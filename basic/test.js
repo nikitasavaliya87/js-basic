8d3908704ccbbc11b501228d389ba1ac72bd4448
Nikita Savaliya <144912665+nikitasavaliya87@users.noreply.github.com>
2025-06-19 04:36:04 +0000

    variable

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,11 @@
-console.log("Nikita")
+//console.log("Nikita")
+const accId=14487
+let accEmail="abc@gmail.com"
+var accPswd="123"
+accState="Gujarat"
+let accCity;
+
+//accId=36
+console.log(accId);
+console.table({accId,accEmail,accPswd,accState,accCity})
+
