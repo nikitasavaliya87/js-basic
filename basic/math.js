fd106eba49fb5f80b577bb98b27f95662f130992
Nikita Savaliya <144912665+nikitasavaliya87@users.noreply.github.com>
2025-06-25 03:51:42 +0000

    number basics

---
+++ basic/math.js
@@ -0,0 +1,14 @@
+const bal=new Number(100)
+const bal1=289.866555
+// console.log(bal1);
+
+// console.log(bal.toFixed(2)); 
+// console.log(bal1.toPrecision(8));
+
+
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString()) // converted no into the comma format in US Style
+// console.log(hundreds.toLocaleString("en-IN"));
+
+ 
